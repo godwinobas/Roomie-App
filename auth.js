4e03dd8af39884290f0ea4330fb65c0193cb50d8
iamMusa <godwinobas0@gmail.com>
2024-05-03 23:11:05 +0100

    render redirect url fix

--- auth.js
+++ auth.js
@@ -18,7 +18,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/callback',
+      callbackURL: 'https://roomie-app-325b.onrender.com/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
       // check if user already exists
