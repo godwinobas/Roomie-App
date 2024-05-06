a87c132e46fcfc6bb8f87723fc3215a6ea048611
iamMusa <godwinobas0@gmail.com>
2024-05-06 20:20:24 +0100

    data return fix

--- routes/routes.js
+++ routes/routes.js
@@ -43,13 +43,10 @@ router.get(
 );
 
 router.get('/auth/success', isLoggedIn, (req, res) => {
-  let user = [
-    { username: req.user.username },
-    {
-      profilePhoto: req.user.picture,
-    },
-    { email: req.user.email },
-  ];
+  let user =
+    ({ username: req.user.username },
+    { profilePhoto: req.user.picture },
+    { email: req.user.email });
   res.json({ data: user });
 });
 
