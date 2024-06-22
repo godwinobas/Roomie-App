9c147227320ac12ec43328f746dbc1b04f461d0e
iamMusa <godwinobas0@gmail.com>
2024-06-22 20:38:01 +0100

    changed google oauth response  to json

--- controllers/authController.js
+++ controllers/authController.js
@@ -88,7 +88,12 @@ export const aSuccess = (req, res, next) => {
         httpOnly: false,
         secure: false, // Set to true in production
       })
-      .redirect(process.env.FRONTENDAPP_URL + '?auth=' + token);
+      .json({
+        message: 'User created successfully',
+        user: req.user,
+        cookie: token,
+      });
+    // .redirect(process.env.FRONTENDAPP_URL + '?auth=' + token);
   } catch (err) {
     console.error(err);
     next(err);
