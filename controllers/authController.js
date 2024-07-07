5bcd46b93543abf3fac5f146fb548caf73ac6ab0
iamMusa <godwinobas0@gmail.com>
2024-07-07 16:42:06 +0100

    reverted to bearer token

--- controllers/authController.js
+++ controllers/authController.js
@@ -82,16 +82,13 @@ export const aSuccess = (req, res, next) => {
     console.log('this is the session cookie ' + req.session.myappUserId);
 
     // Set the JWT as an HTTP-only cookie
-    res.cookie('myCookie', token, {
-      maxAge: 3600000, // 1 hour (adjust as needed)
-      httpOnly: false,
-      secure: false, // Set to true in production
-    });
-    res.status(200).json({
-      message: 'user autenticated in successfully',
-      user: req.user,
-      cookie: token,
-    });
+    res
+      .cookie('myCookie', token, {
+        maxAge: 3600000, // 1 hour (adjust as needed)
+        httpOnly: false,
+        secure: true, // Set to true in production
+      })
+      .redirect(process.env.FRONTENDAPP_URL + '?auth=' + req.user);
   } catch (err) {
     console.error(err);
     next(err);
